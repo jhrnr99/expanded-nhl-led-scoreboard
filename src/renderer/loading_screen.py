9231b3c813edabec88362dff83d8ea4464eee2ae
Jacob Horner <horner.jacob11@gmail.com>
2023-10-23 19:01:32 -0500

    Trying to make loading screen a gif

--- src/renderer/loading_screen.py
+++ src/renderer/loading_screen.py
@@ -1,16 +1,35 @@
 from PIL import Image
 from images.image_helper import ImageHelper
 
-PATH = "assets/loading/loading.png"
+PATH = "assets/loading/loading.gif"
 
 class Loading:
     def __init__(self, matrix):
         self.matrix = matrix
-        self.image = Image.open(PATH)
-    def render(self):
 
-        self.matrix.draw_image(
-            (0,0),
-            self.image
-        )
-        self.matrix.render()
+    def play_gif(self, file):
+        im = Image.open(PATH)
+
+        # Set the frame index to 0
+        frame_nub = 0
+        # Set number of loop to 1 (if you want to play you animation more then once, change this variable)
+        numloop = 5
+        self.matrix.clear()
+
+        # Go through the frames
+        x = 0
+        while x is not numloop:
+            try:
+                im.seek(frame_nub)
+            except EOFError:
+                x += 1
+                if x == numloop:
+                    return
+                frame_nub = 0
+                im.seek(frame_nub)
+
+            self.matrix.draw_image(("50%", 0), im, "center")
+            self.matrix.render()
+
+            frame_nub += 1
+            self.sleepEvent.wait(0.1)
